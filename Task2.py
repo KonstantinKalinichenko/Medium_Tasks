a34a812a2bc882f3ff2b745bc8a665af975130d7
Konstantin Kalinichenko <152182945+KonstantinKalinichenko@users.noreply.github.com>
2024-12-04 12:24:24 +0200

    First thoughts about future solution

--- Task2.py
+++ Task2.py
@@ -1,4 +1,20 @@
+from collections import defaultdict
+
+
 def sort_products(input_strings) -> list:
+    splitted = input_strings[0].split()
+    products = [item.split(',') for item in splitted]
+    dict_products  = defaultdict(list)
+    for product in products:
+        dict_products[product[1]].append([product[0],
+                                          str(float(product[2]))])
+    sorted_products = dict(sorted(dict_products.items()))
+    for category in sorted_products:
+        sorted_products[category] = sorted(
+            sorted_products[category],
+            key=lambda x: (x[1], x[0])
+        )
+    print(sorted_products)
 
 
 lines = []
@@ -11,5 +27,6 @@ while True:
       break
    lines.append(line)
 
-for string in sort_products(lines):
-   print(string)
+print(sort_products(lines))
+# for string in sort_products(lines):
+#    print(string)
