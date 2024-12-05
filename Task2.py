4693e5a1af567c05e21bb18d3f5f743aa7c214ac
Konstantin Kalinichenko <152182945+KonstantinKalinichenko@users.noreply.github.com>
2024-12-05 10:38:21 +0200

    Added Full solution for the second task.

--- Task2.py
+++ Task2.py
@@ -6,15 +6,17 @@ def sort_products(input_strings) -> list:
     products = [item.split(',') for item in splitted]
     dict_products  = defaultdict(list)
     for product in products:
-        dict_products[product[1]].append([product[0],
-                                          str(float(product[2]))])
+        dict_products[product[1]].append(
+            f'{product[0]},{str(float(product[2]))}'
+        )
     sorted_products = dict(sorted(dict_products.items()))
     for category in sorted_products:
         sorted_products[category] = sorted(
             sorted_products[category],
             key=lambda x: (x[1], x[0])
         )
-    print(sorted_products)
+    result = [f"{key}: {', '.join(value)}" for key, value in sorted_products.items()]
+    return result
 
 
 lines = []
@@ -27,6 +29,6 @@ while True:
       break
    lines.append(line)
 
-print(sort_products(lines))
-# for string in sort_products(lines):
-#    print(string)
+
+for string in sort_products(lines):
+   print(string)
