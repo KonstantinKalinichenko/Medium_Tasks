3f8171e320736ca03aeb10b6180f57d98f8f95c4
Konstantin Kalinichenko <152182945+KonstantinKalinichenko@users.noreply.github.com>
2024-12-07 12:22:11 +0200

    1) Task4 conditions added to README 2) Template solution for Task4 added

---
+++ Task4.py
@@ -0,0 +1,13 @@
+import re
+
+
+class TextProcessor:
+    def __init__(self, text):
+        self.text = text
+
+    def extract_emails(self):
+        """Ваш код"""
+        pass
+
+input_text = input()
+"""Ваш код"""
